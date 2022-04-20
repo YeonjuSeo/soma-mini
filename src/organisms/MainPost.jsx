f6475658ce9670b95e6a66b0f06dbbc745a1d8f7
YeonjuSeo <tjduswn1219@gmail.com>
2022-04-21 00:46:53 +0900

    💄 Add display:flex for post header

--- src/organisms/MainPost.jsx
+++ src/organisms/MainPost.jsx
@@ -4,10 +4,10 @@ import RoundProfile from "../atoms/RoundProfile";
 const MainPost = () => {
   return (
     <Wrapper>
-      <div>
+      <PostHeaderWrapper>
         <RoundProfile w={"32px"} h={"32px"} url={"/assets/Profile6.jpeg"} />
         <div>아이디</div>
-      </div>
+      </PostHeaderWrapper>
       <PostImg src={"/assets/Post5.jpeg"} />
     </Wrapper>
   );
@@ -17,6 +17,9 @@ export default MainPost;
 const Wrapper = styled.div`
   background-color: white;
   border: 0.5px solid rgba(239, 239, 239, 1);
+`;
+const PostHeaderWrapper = styled.div`
+  display: flex;
 `;
 const PostImg = styled.img`
   width: 100%;
