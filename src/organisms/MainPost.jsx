e47d72a20e8e857dcdb3940156b6de83c610947d
YeonjuSeo <tjduswn1219@gmail.com>
2022-04-21 00:40:32 +0900

    🚧  Add Post Component of Main

---
+++ src/organisms/MainPost.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "@emotion/styled";
+import RoundProfile from "../atoms/RoundProfile";
+const MainPost = () => {
+  return (
+    <Wrapper>
+      <div>
+        <RoundProfile w={"32px"} h={"32px"} url={"/assets/Profile6.jpeg"} />
+        <div>아이디</div>
+      </div>
+      <PostImg src={"/assets/Post5.jpeg"} />
+    </Wrapper>
+  );
+};
+export default MainPost;
+
+const Wrapper = styled.div`
+  background-color: white;
+  border: 0.5px solid rgba(239, 239, 239, 1);
+`;
+const PostImg = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 614px;
+  max-height: 614px;
+`;
