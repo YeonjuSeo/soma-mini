097716bef5f6966acff5284b54c30719f9e42354
YeonjuSeo <tjduswn1219@gmail.com>
2022-04-21 01:59:32 +0900

    💄  Add More Css for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,21 +10,33 @@ import RoundProfile from "../atoms/RoundProfile";
 const Header = () => {
   return (
     <Wrapper>
-      <div>Sotagram</div>
-      <input placeholder={"검색"} />
-      <FuncsWrapper>
-        <MdHomeFilled style={{ width: "24px", height: "24px" }} />
-        <HiOutlinePaperAirplane style={{ width: "24px", height: "24px" }} />
-        <FiPlusSquare style={{ width: "24px", height: "24px" }} />
-        <ImCompass2 style={{ width: "24px", height: "24px" }} />
-        <HiOutlineHeart style={{ width: "24px", height: "24px" }} />
-        <RoundProfile
-          alt={"Profile Img - Yawning Cat"}
-          w={"24px"}
-          h={"24px"}
-          url={ProfileImg}
-        />
-      </FuncsWrapper>
+      <ElementsWrapper>
+        <Logo>Somagram</Logo>
+        <StyledInput placeholder={"검색"} />
+        <FuncsWrapper>
+          <MdHomeFilled
+            style={{ width: "24px", height: "24px", marginRight: "14px" }}
+          />
+          <HiOutlinePaperAirplane
+            style={{ width: "24px", height: "24px", marginRight: "14px" }}
+          />
+          <FiPlusSquare
+            style={{ width: "24px", height: "24px", marginRight: "14px" }}
+          />
+          <ImCompass2
+            style={{ width: "24px", height: "24px", marginRight: "14px" }}
+          />
+          <HiOutlineHeart
+            style={{ width: "24px", height: "24px", marginRight: "14px" }}
+          />
+          <RoundProfile
+            alt={"Profile Img - Yawning Cat"}
+            w={"24px"}
+            h={"24px"}
+            url={ProfileImg}
+          />
+        </FuncsWrapper>
+      </ElementsWrapper>
     </Wrapper>
   );
 };
@@ -34,7 +46,25 @@ const Wrapper = styled.div`
   background-color: white;
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  height: 60px;
+  align-items: center;
 `;
-
+const Logo = styled.div`
+  font-weight: bold;
+`;
+const StyledInput = styled.input`
+  border: none;
+  background-color: #efefef;
+  border-radius: 8px;
+  padding: 0 16px;
+  width: 228px;
+`;
+const ElementsWrapper = styled.div`
+  max-width: 975px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
 const FuncsWrapper = styled.div``;
