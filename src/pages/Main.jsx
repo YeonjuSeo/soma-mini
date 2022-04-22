67b3ec4a666db31d6a8291e16a42d87a05d8c503
YeonjuSeo <tjduswn1219@gmail.com>
2022-04-22 11:30:35 +0900

    ✨  Add Carousel for Story of Main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,33 @@
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
+// @import url("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css");
+// @import url("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css");
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 import styled from "@emotion/styled";
 import Header from "../components/Header";
 import Story from "../components/Story";
 import MainPost from "../organisms/MainPost";
 import data from "../assets/data.json";
+import Slider from "react-slick";
+
 const Main = () => {
   return (
     <Wrapper>
       <Header />
       <ContentWrapper>
         <StoryWrapper>
-          {data.story.map((el) => (
-            <Story id={el.id} url={el.url} />
-          ))}
+          <Slider
+            infinite={false}
+            draggable={false}
+            slidesToScroll={7}
+            slidesToShow={7}
+          >
+            {data.story.map((el) => (
+              <Story id={el.id} url={el.url} />
+            ))}
+          </Slider>
         </StoryWrapper>
         {data.post.map((el) => (
           <MainPost
@@ -31,7 +46,6 @@ const Main = () => {
 export default Main;
 
 const Wrapper = styled.div`
-  height: 100%;
   background-color: #fafafa;
   display: flex;
   flex-direction: column;
@@ -42,9 +56,9 @@ const ContentWrapper = styled.div`
   max-width: 614px;
 `;
 const StoryWrapper = styled.div`
-  display: flex;
   margin-bottom: 24px;
   background-color: white;
-  border: 0.5px solid rgba(239, 239, 239, 1);
-  padding: 16px 10px;
+  border: 1px solid rgba(239, 239, 239, 1);
+  padding: 16px 0;
+  width: 100%;
 `;
