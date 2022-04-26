a0487e9ffa6ef4af9f9e2284306def55ed929bec
노희찬 <nhc7502@naver.com>
2022-04-27 02:03:10 +0900

    Add Story

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Main from "./pages/Main";
+import Story from "./pages/Story";
 const Root = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/stories/" element={<Story />} />
       </Routes>
     </>
   );
---
+++ src/pages/Story.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "@emotion/styled";
+const Story = () => {
+  return (
+    <StoryWrapper>
+    </StoryWrapper>
+  );
+};
+export default Story;
+
+const StoryWrapper = styled.div`
+  height: 100%;
+  background-color: orange;
+`;
