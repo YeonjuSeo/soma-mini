878bb7d298f80c056841da6a20e41a6d591f8564
노희찬 <nhc7502@naver.com>
2022-04-28 14:33:56 +0900

    Implement Heart

--- src/components/StoryPostFooter.jsx
+++ src/components/StoryPostFooter.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 import { HiHeart, HiOutlineHeart, HiOutlinePaperAirplane } from "react-icons/hi";
 
 const StoryPostFooter = ({ id }) => {
+  const [heart, setHeart] = useState(false);
+  const [color, setColor] = useState("#FFFFFF");
   let replyString = `${id}님에게 답장하기...`;
+
+  const RenderHeart = () => {
+    if (heart) {
+      return (
+        <HiHeart
+          style={IconStyle(color)}
+          onClick={() => {
+            setHeart(!heart);
+          }}
+        />
+      );
+    }
+    else {
+      return (
+        <HiOutlineHeart
+          style={IconStyle(color)}
+          onClick={() => {
+            setColor("#ED4956")
+            setHeart(!heart);
+          }}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      )
+    }
+  };
+
+  const onMouseEnter = () => {
+    setColor("#8E8E8E")
+  }
+  const onMouseLeave = () => {
+    setColor("#FFFFFF")
+  }
 
   return (
     <Wrapper>
       <ReplyInput placeholder = {replyString} />
-      <HiOutlineHeart style={IconStyle} />
-      <HiOutlinePaperAirplane style={IconStyle} />
+      {RenderHeart()}
+      <HiOutlinePaperAirplane style={IconStyle("#FFFFFF")} />
     </Wrapper>
   );
 };
@@ -32,10 +67,9 @@ const ReplyInput = styled.input`
   color: #8A8786;
   padding: 0px 11px;
 `;
-const IconStyle = {
+const IconStyle = (color) => { return {
   width: "24px",
   height: "24px",
   cursor: "grab",
-  color: "#FFFFFF"
-  // TODO: 하트 클릭시 #ED4956
-};
+  color: color
+}};
